64b1fd6d1c44de6eb952c0d43f663fa364432339
David Yoon <david.hyeshik@yahoo.com>
2021-08-17 10:05:47 +0900

    Update 7.lb.tf

--- regionB/7.lb.tf
+++ regionB/7.lb.tf
@@ -1,7 +1,7 @@
 resource "azurerm_lb" "user30-lb" {
   name                    = "user30lb"
-  location                = azurerm_resource_group.user30-rg1.location
-  resource_group_name     = azurerm_resource_group.user30-rg1.name
+  location                = azurerm_resource_group.user30-rg2.location
+  resource_group_name     = azurerm_resource_group.user30-rg2.name
   
   frontend_ip_configuration {
     name                  = "user30PublicIPAddress"
