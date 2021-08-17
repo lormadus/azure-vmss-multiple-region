8944738a359a99bcdc56e91e93efffd8cf8f3b4e
David Yoon <david.hyeshik@yahoo.com>
2021-08-17 10:06:12 +0900

    Update 8.lb_backendpool.tf

--- regionB/8.lb_backendpool.tf
+++ regionB/8.lb_backendpool.tf
@@ -1,4 +1,4 @@
-resource "azurerm_lb_backend_address_pool" "user30-bpepool" {
+resource "azurerm_lb_backend_address_pool" "user30-bep" {
     name = "user30-BackEndAddressPool"
     loadbalancer_id     = azurerm_lb.user30-lb.id
 }
