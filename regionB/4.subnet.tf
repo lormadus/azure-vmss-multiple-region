8de4739da61f9be9d96816c63d78c6a2d9b95ac7
David Yoon <david.hyeshik@yahoo.com>
2021-08-17 10:04:29 +0900

    Update 4.subnet.tf

--- regionB/4.subnet.tf
+++ regionB/4.subnet.tf
@@ -1,6 +1,6 @@
 resource "azurerm_subnet" "user30-subnet1" {
     name 		= "user30-mysubnet1"
-    resource_group_name = azurerm_resource_group.user30-rg1.name
+    resource_group_name = azurerm_resource_group.user30-rg2.name
     virtual_network_name= azurerm_virtual_network.user30-vnet.name
     address_prefixes 	= ["130.0.1.0/24"]
 }
