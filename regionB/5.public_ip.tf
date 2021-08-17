9a95c59c7b336f0fcfcfd5e46fc971143e84832a
David Yoon <david.hyeshik@yahoo.com>
2021-08-17 10:04:49 +0900

    Update 5.public_ip.tf

--- regionB/5.public_ip.tf
+++ regionB/5.public_ip.tf
@@ -1,9 +1,9 @@
 resource "azurerm_public_ip" "user30-publicip" {  
 name                = "mypublicIP"  
-location            = azurerm_resource_group.user30-rg1.location  
-resource_group_name = azurerm_resource_group.user30-rg1.name  
+location            = azurerm_resource_group.user30-rg2.location  
+resource_group_name = azurerm_resource_group.user30-rg2.name  
 allocation_method   = "Static"  
-domain_name_label   = azurerm_resource_group.user30-rg1.name  
+domain_name_label   = azurerm_resource_group.user30-rg2.name  
 	
 	tags = {    
 		environment = "staging"  
